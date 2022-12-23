369e1f77280e196b73c6c45b7a8f3f2b881fd275
nat-tsang <111996917+nat-tsang@users.noreply.github.com>
2022-12-23 10:24:18 -0800

    update

---
+++ strava_wrapped/StravaWrapped/plots.py
@@ -0,0 +1,11 @@
+import plotly.graph_objects as go
+from plotly.graph_objs import Scatter
+import plotly.express as px
+import pandas as pd
+import datetime as dt
+
+class Plots():
+    def distance_date(df):
+        activity_df = df
+        
+
